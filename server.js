ac3df940a3e4f2d748dfc0fe72ed7b7442af88ee
Roland Barenbrug <roland@vm-7.mynet.lan>
2019-02-15 23:30:14 +0100

    Added setzero support for tinyg

--- server.js
+++ server.js
@@ -2039,25 +2039,25 @@ io.sockets.on('connection', function (appSocket) {
         if (isConnected) {
             switch (data) {
             case 'x':
-                if (firmware == "marlin" || firmware == "reprapfirmware")
+                if (firmware == "marlin" || firmware == "reprapfirmware" || firmware == "tinyg")
                     addQ('G92 X0');
                 else
                     addQ('G10 L20 P0 X0');
                 break;
             case 'y':
-                if (firmware == "marlin" || firmware == "reprapfirmware")
+                if (firmware == "marlin" || firmware == "reprapfirmware" || firmware == "tinyg")
                     addQ('G92 Y0');
                 else
                     addQ('G10 L20 P0 Y0');
                 break;
             case 'z':
-                if (firmware == "marlin" || firmware == "reprapfirmware")
+                if (firmware == "marlin" || firmware == "reprapfirmware" || firmware == "tinyg")
                     addQ('G92 Z0');
                 else
                     addQ('G10 L20 P0 Z0');
                 break;
             case 'a':
-                if (firmware == "marlin" || firmware == "reprapfirmware")
+                if (firmware == "marlin" || firmware == "reprapfirmware" || firmware == "tinyg")
                     addQ('G92 E0');
                 else
                     addQ('G10 L20 P0 A0');
@@ -2070,6 +2070,7 @@ io.sockets.on('connection', function (appSocket) {
                 case 'marlinkimbra':
                 case 'marlin':
                 case 'reprapfirmware':
+                case 'tinyg':
                     addQ('G92 X0 Y0 Z0');
                     break;
                 default:
@@ -2078,7 +2079,7 @@ io.sockets.on('connection', function (appSocket) {
                 }
                 break;
             case 'xyza':
-                if (firmware == "marlin" || firmware == "reprapfirmware")
+                if (firmware == "marlin" || firmware == "reprapfirmware"  || firmware == "tinyg")
                     addQ('G92 X0 Y0 Z0 E0');
                 else
                     addQ('G10 L20 P0 X0 Y0 Z0 A0');
